e3b79dbd5920510dec7fe8ce2e87f2bfe8764739
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2024-06-12 14:51:07 +0530

    update and delete routes added

--- src/routes/outward.routes.js
+++ src/routes/outward.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { JWTcheck } from "../middlewares/auth.middleware.js";
 import { allAdminCheck } from "../middlewares/allAdmin.middleware.js";
-import { getAllOutwards, outwardCreationRequest, outwardReturnRequest } from "../controllers/outward.controller.js";
+import { deleteOutward, getAllOutwards, outwardCreationRequest, outwardReturnRequest, updateOutward } from "../controllers/outward.controller.js";
 import { AssetAdmincheck } from "../middlewares/assetAdmin.middleware.js";
 import { SecurityAdmincheck } from "../middlewares/securityAdmin.middleware.js";
 
@@ -10,5 +10,7 @@ const router = Router()
 router.route("/").get(JWTcheck,allAdminCheck,getAllOutwards)
 router.route("/addRequest").post(JWTcheck,AssetAdmincheck,outwardCreationRequest) //Here we are expecting an asset ID
 router.route("/returnRequest").post(JWTcheck,SecurityAdmincheck,outwardReturnRequest) //Here we are expecting outward ID
+router.route("/update/:id").post(JWTcheck,SecurityAdmincheck,updateOutward)
+router.route("/delete/:id").delete(JWTcheck,SecurityAdmincheck,deleteOutward)
 
 export default router
