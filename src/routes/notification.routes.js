bc3c7b2d0aef637f2b2e536a4e8fb398df2c209a
Abhijeet Chauhan <aschauhan1052002@gmail.com>
2024-06-23 11:54:09 +0530

    isRead route added

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { JWTcheck } from "../middlewares/auth.middleware.js";
-import { getNotificationsForUser } from "../controllers/notification.controllers.js";
+import { getNotificationsForUser, makeItRead } from "../controllers/notification.controllers.js";
 
 const router = Router();
 
 router.route("/getNotifications").get(JWTcheck,getNotificationsForUser)
+
+router.route("/isRead").post(JWTcheck,makeItRead)
 
 /*
 Notifs will be created internally when required by directly using the controller provided and
