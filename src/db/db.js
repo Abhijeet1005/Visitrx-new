1d6b5ff66cd201a265a8b81670e84493accb3ac0
Abhijeet1005 <aschauhan1052002@gmail.com>
2024-08-27 19:39:51 +0530

    Changes related to testing.

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
 
-const connectDB = async ()=>{
-    try {
-        const connectionData = await mongoose.connect(`${process.env.MONGO}/${process.env.DB_NAME}`)
-        // console.log(`MongoDB connected with host: ${connectionData.connection.host}`)
-        return connectionData.connection.host
-    } catch (error) {
-        // console.log('MongoDB connection error', error)
-        throw error
-        process.exit(1)
+const connectDB = async () => {
+  try {
+    let connectionString;
+
+    if (process.env.NODE_ENV === "test") {
+      connectionString = `${process.env.TESTING_MONGO}`;
+      console.log("-- Connecting to testing database --");
+    } else {
+      connectionString = `${process.env.MONGO}/${process.env.DB_NAME}`;
+      console.log("-- Connecting to main database --");
     }
-}
+
+    const connectionData = await mongoose.connect(connectionString);
+    // console.log(`MongoDB connected with host: ${connectionData.connection.host}`)
+    return connectionData.connection.host;
+  } catch (error) {
+    // console.log('MongoDB connection error', error)
+    throw error;
+    process.exit(1);
+  }
+};
 export default connectDB;
